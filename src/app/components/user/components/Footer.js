b8562626ac8b04ed42c4b70cbad4edb798bc8a16
rupali sharma <rs8898087@gmail.com>
2024-12-15 14:29:09 +0000

    footage changes

--- src/app/components/user/components/Footer.js
+++ src/app/components/user/components/Footer.js
@@ -1,30 +1,149 @@
-import React from 'react'
-import Image from 'next/image';
-
+import React from "react";
+import Image from "next/image";
 
 export default function Footer() {
-    return (
-        <>
+  return (
+    <>
+      <div className="footer-main-div">
+        <div className="footer-first-section"></div>
+        <div className="footer-second-section">
+          <div className="row">
+            <div className="col-md-6">
+              <div className="foot-left">
+                <p className="bcbcbc-text ">
+                  All screenshots © of their respective owners.
+                </p>
+                <div className="social-links">
+                  <div className="linkedin-svg">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M24 24V15.21C24 10.89 23.07 7.59 18.03 7.59C15.6 7.59 13.98 8.91 13.32 10.17H13.26V7.98H8.49V24H13.47V16.05C13.47 13.95 13.86 11.94 16.44 11.94C18.99 11.94 19.02 14.31 19.02 16.17V23.97H24V24ZM0.39 7.98H5.37V24H0.39V7.98ZM2.88 0C1.29 0 0 1.29 0 2.88C0 4.47 1.29 5.79 2.88 5.79C4.47 5.79 5.76 4.47 5.76 2.88C5.76 1.29 4.47 0 2.88 0Z"
+                        fill="#8E8E8E"
+                      />
+                    </svg>
+                  </div>
 
-            <div className="footer-main-div">
-                <div className='footer-first-section'>
+                  <div className="twitter-svg">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clip-path="url(#clip0_480_5587)">
+                        <path
+                          d="M14.2339 10.1624L22.9764 0H20.9047L13.3137 8.82384L7.2507 0H0.257812L9.42618 13.3432L0.257812 24H2.3296L10.3459 14.6817L16.7489 24H23.7418L14.2334 10.1624H14.2339ZM11.3963 13.4608L10.4674 12.1321L3.0761 1.55961H6.25825L12.2231 10.0919L13.1521 11.4206L20.9057 22.5113H17.7235L11.3963 13.4613V13.4608Z"
+                          fill="#8E8E8E"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_480_5587">
+                          <rect width="24" height="24" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </div>
 
-                         
+                  <div className="instagram-svg">
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <g clip-path="url(#clip0_480_5592)">
+                        <path
+                          d="M17.9996 0H5.99985C2.70023 0 0 2.70023 0 5.99985V18.0001C0 21.2989 2.70023 24 5.99985 24H17.9996C21.2992 24 23.9994 21.2989 23.9994 18.0001V5.99985C23.9994 2.70023 21.2992 0 17.9996 0ZM21.9994 18.0001C21.9994 20.2051 20.2057 22 17.9996 22H5.99985C3.79461 22 2.00005 20.2051 2.00005 18.0001V5.99985C2.00005 3.79432 3.79461 2.00005 5.99985 2.00005H17.9996C20.2057 2.00005 21.9994 3.79432 21.9994 5.99985V18.0001Z"
+                          fill="#8E8E8E"
+                        />
+                        <path
+                          d="M18.5 6.99993C19.3284 6.99993 19.9999 6.32837 19.9999 5.49996C19.9999 4.67156 19.3284 4 18.5 4C17.6716 4 17 4.67156 17 5.49996C17 6.32837 17.6716 6.99993 18.5 6.99993Z"
+                          fill="#8E8E8E"
+                        />
+                        <path
+                          d="M11.9999 6C8.68552 6 6 8.68581 6 11.9999C6 15.3127 8.68552 18.0003 11.9999 18.0003C15.3132 18.0003 17.9997 15.3127 17.9997 11.9999C17.9997 8.68581 15.3132 6 11.9999 6ZM11.9999 16.0002C9.79098 16.0002 8.00005 14.2093 8.00005 11.9999C8.00005 9.7904 9.79098 8.00005 11.9999 8.00005C14.2087 8.00005 15.9997 9.7904 15.9997 11.9999C15.9997 14.2093 14.2087 16.0002 11.9999 16.0002Z"
+                          fill="#8E8E8E"
+                        />
+                      </g>
+                      <defs>
+                        <clipPath id="clip0_480_5592">
+                          <rect width="24" height="24" fill="white" />
+                        </clipPath>
+                      </defs>
+                    </svg>
+                  </div>
                 </div>
-                <div className='footer-second-section'>
-
-                    <div className='row'> 
-
-                            <div className='col-md-6'> 
-                                left section 
-                            </div>
-                            <div className='col-md-6'>
-
-                                Right section 
-                            </div>
-                    </div>
-                </div>
+              </div>
             </div>
-        </>
-    )
+            <div className="col-md-6">
+              <div className="foot-right">
+                <div className="foot-list">
+                  <ul>
+                    <li>
+                      <a className="aeaeae-text" href="">
+                        About us
+                      </a>
+                    </li>
+                    <li>
+                      <a className="aeaeae-text" href="">
+                        Read our blog
+                      </a>
+                    </li>
+                    <li>
+                      <a className="aeaeae-text" href="">
+                        Contact Us
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="foot-list">
+                  <ul>
+                    <li>
+                      <a className="aeaeae-text" href="">
+                        Pricing
+                      </a>
+                    </li>
+                    <li>
+                      <a className="aeaeae-text" href="">
+                        Privacy policy
+                      </a>
+                    </li>
+                    <li>
+                      <a className="aeaeae-text" href="">
+                        Terma & Conditions
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+
+                <div className="foot-list">
+                  <ul>
+                    <li>
+                      <a className="aeaeae-text" href="">
+                        FAQ
+                      </a>
+                    </li>
+                    <li>
+                      <a className="aeaeae-text" href="">
+                        Blogs
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
